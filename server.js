3435c5986fb9e753a8d7c9a2d20fda4c13ffcc19
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-10 12:49:40 +0530

    Testing 2

--- server.js
+++ server.js
@@ -3,37 +3,62 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 
-// Create a new express application
-const app = express();
+// Create two express applications
+const app1 = express();
+const app2 = express();
 
-// Create a new http server
-const server = http.createServer(app);
+// Create two http servers
+const server1 = http.createServer(app1);
+const server2 = http.createServer(app2);
 
-// Create a new socket.io instance
-const io = socketio(server);
+// Create two socket.io instances
+const io1 = socketio(server1);
+const io2 = socketio(server2);
 
 // Serve the static files from the public folder
-app.use(express.static("public"));
+app1.use(express.static("public"));
+app2.use(express.static("public"));
 
-// Listen for new connections
-io.on("connection", (socket) => {
-  console.log("A user connected");
+// Listen for new connections on server1
+io1.on("connection", (socket) => {
+  console.log("A user connected to server1");
 
-  // Listen for chat messages
+  // Listen for chat messages on server1
   socket.on("chat message", (msg) => {
-    console.log("Message: " + msg);
+    console.log("Message from server1: " + msg);
 
-    // Broadcast the message to all other users
-    socket.broadcast.emit("chat message", msg);
+    // Broadcast the message to all other users on server2
+    io2.emit("chat message", msg);
   });
 
-  // Listen for disconnections
+  // Listen for disconnections on server1
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    console.log("A user disconnected from server1");
   });
 });
 
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+// Listen for new connections on server2
+io2.on("connection", (socket) => {
+  console.log("A user connected to server2");
+
+  // Listen for chat messages on server2
+  socket.on("chat message", (msg) => {
+    console.log("Message from server2: " + msg);
+
+    // Broadcast the message to all other users on server1
+    io1.emit("chat message", msg);
+  });
+
+  // Listen for disconnections on server2
+  socket.on("disconnect", () => {
+    console.log("A user disconnected from server2");
+  });
+});
+
+// Start the servers on different ports
+server1.listen(3000, () => {
+  console.log("Server1 listening on port 3000");
+});
+server2.listen(3001, () => {
+  console.log("Server2 listening on port 3001");
 });
